62bad9a9fc629c3064574b89d17bfe44b960245c
Filip Grace <filip@filbot.com>
2021-06-17 09:00:18 -0700

    add alt prop

--- src/Components/AttractionsHeader.js
+++ src/Components/AttractionsHeader.js
@@ -5,9 +5,9 @@ const AttractionsHeader = () => {
 	return (
 		<div className="attractions-header-container">
 			<div className="attraction-header-title">
-				<div className="flourish"><img src={flourishLeft} /></div>
+				<div className="flourish"><img src={flourishLeft} alt=""/></div>
 				<div className="title-text">ATTRACTIONS</div>
-				<div className="flourish"><img src={flourishRight} /></div>
+				<div className="flourish"><img src={flourishRight} alt=""/></div>
 			</div>
 			<div className="attraction-header-subtitle">
 				<div className="top">Wait</div>
