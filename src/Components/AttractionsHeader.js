2f1a297467aa15f47b8b0520283be401d1025e45
Filip Grace <filip@filbot.com>
2021-06-17 08:18:54 -0700

    add assets and sizing

--- src/Components/AttractionsHeader.js
+++ src/Components/AttractionsHeader.js
@@ -1,7 +1,7 @@
 const AttractionsHeader = () => {
 	return (
 		<div className="attractions-header-container">
-			<div className="attraction-header-title">Attractions</div>
+			<div className="attraction-header-title">ATTRACTIONS</div>
 			<div className="attraction-header-subtitle">
 				<div className="top">Wait</div>
 				<div className="bottom">Times</div>
--- src/Components/ParkHours.js
+++ src/Components/ParkHours.js
@@ -1,10 +1,12 @@
 const ParkHours = () => {
 	return (
 		<div className="park-hours-container">
-			<div className="title">Park Hours</div>
-			<div className="time-container">
-				<div className="top-time">8:00am</div>
-				<div className="bottom-time">11:00pm</div>
+			<div className="green-container">
+				<div className="title">Park Hours</div>
+				<div className="time-container">
+					<div className="top-time">8:00am</div>
+					<div className="bottom-time">11:00pm</div>
+				</div>
 			</div>
 		</div>
 	)
