cf8e378838834284b5515b685847d6f399eb334d
Filip Grace <filip@filbot.com>
2021-06-17 15:01:30 -0700

    test cors issues

--- src/App.js
+++ src/App.js
@@ -1,16 +1,30 @@
 import './App.css';
+import React, { useState, useEffect } from 'react';
 import Header from './Components/Header';
 import ParkHours from './Components/ParkHours';
 import AttractionsHeader from './Components/AttractionsHeader';
 import Attraction from './Components/Attraction';
 
 function App() {
+  const [attractions, setAttractions] = useState([]);
+
+  useEffect(() => {
+    const getApiData = async () => {
+      const response = await fetch('https://cat-fact.herokuapp.com/facts');
+      const responseJson = await response.json();
+      setAttractions(JSON.stringify(responseJson));
+    }
+
+    getApiData();
+  }, []);
+
   return (
     <div className="App">
       <Header />
       <ParkHours />
       <AttractionsHeader />
       <Attraction />
+      <div id="data">{attractions}</div>
     </div>
   );
 }
