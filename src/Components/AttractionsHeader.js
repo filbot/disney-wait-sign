acb4b74768848c60bbd376feb82ae6ff0bd396d3
Filip Grace <filip@filbot.com>
2021-06-17 08:46:13 -0700

    attractions sizing

--- src/Components/AttractionsHeader.js
+++ src/Components/AttractionsHeader.js
@@ -1,7 +1,14 @@
+import flourishLeft from '../flourish-left.png';
+import flourishRight from '../flourish-right.png';
+
 const AttractionsHeader = () => {
 	return (
 		<div className="attractions-header-container">
-			<div className="attraction-header-title">ATTRACTIONS</div>
+			<div className="attraction-header-title">
+				<div className="flourish"><img src={flourishLeft} /></div>
+				<div className="title-text">ATTRACTIONS</div>
+				<div className="flourish"><img src={flourishRight} /></div>
+			</div>
 			<div className="attraction-header-subtitle">
 				<div className="top">Wait</div>
 				<div className="bottom">Times</div>
