b79e120dff2a276b0168b4f7837688bd3a587312
Filip Grace <filip.grace@toyota.com>
2024-10-21 10:42:28 -0700

    Refactor header styling and add Disneyland svg logo

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,5 +1,11 @@
+import logo from '../assets/disneyland-logo.svg'; // Adjust the path as necessary
+
 const Header = () => {
-	return <header>Disneyland</header>
+  return (
+    <header>
+      <img src={logo} alt="Disneyland Logo" />
+    </header>
+  );
 }
 
 export default Header;
